a187330b0e7ef586c2ec0ca380f14d98cf265e1a
Rosario Lorenzo <rochi.lorenzo@gmail.com>
2020-08-25 18:02:03 -0300

    navSide ready

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let logoNav = document.getElementById('logoNav').addEventListener('click', function(){
+
+    let sideNav = document.getElementById('sideNav');
+
+    sideNav.style.left= '0';
+
+
+
+})

8f8c5887efa75eaeae6d5a2439917eb284e07e57
Rosario Lorenzo <rochi.lorenzo@gmail.com>
2020-08-29 23:07:40 -0300

    changes flex content banner and dark background in dropdown

--- js/index.js
+++ js/index.js
@@ -1,10 +1,7 @@
-
 let pageWrapper = document.getElementById('page-wrapper');
-let sideNavOpen = false;
 document.getElementById('logoNav').addEventListener('click',toggleSidebar)
 
 function toggleSidebar() {
     pageWrapper.classList.toggle('open-sidebar');
+ 
 }
-
-

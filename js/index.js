d9786a2022ee7dac7fa9ddde87d5e849661ea45d
Rosario Lorenzo <rochi.lorenzo@gmail.com>
2020-08-30 21:07:32 -0300

    navside ready

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 let pageWrapper = document.getElementById('page-wrapper');
 document.getElementById('logoNav').addEventListener('click',toggleSidebar)
+let body = document.getElementsByTagName('body')[0]
 
 function toggleSidebar() {
     pageWrapper.classList.toggle('open-sidebar');
- 
+    body.classList.toggle('hide-overflow')
 }
+let bodysite = document.getElementById('body-site');
+let isSidebarOpen = () => pageWrapper.className.includes('open-sidebar');
+bodysite.addEventListener('click', () => {
+    if(isSidebarOpen()) {
+        toggleSidebar()
+    }
+})
